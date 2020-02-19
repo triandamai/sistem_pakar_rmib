70aae240265e8cd57d4536d07998145c645fe44e
Trian Damai <triannurizkillah@gmail.com>
2020-02-19 10:53:33 +0700

    updated batch 3

--- application/views/User/mulai-analisa.php
+++ application/views/User/mulai-analisa.php
@@ -16,6 +16,11 @@
                     
                     
                     ?>).</h5>
+                    <script type="text/javascript">
+                      var no= [false, false, false,false, false, false, false, false, false, false, false, false];
+
+                     // console.log(no);
+                    </script>
                     <p class="card-header">
                       Isi form dibawah dengan mengisi angka 1 sampai 12.
                     </p>
@@ -29,25 +34,31 @@
                            <div class="col-3 col-lg-3">
                             <input type="hidden" name="tabel" value="<?= $_GET['tabel'];?>">
                             <input id="inputUserName" type="hidden" name="id_sub[]" value="<?= $si->id;?>">
-                            <input id="i<?= $no;?>"  onkeydown="change(this.value,<?= $no;?>);" onkeyup="change(this.value,<?= $no;?>);" type="number" name="val[]" data-parsley-trigger="change" required placeholder="max : 12" autocomplete="off" class="form-control">
-                            <p style="color:red;margin-left=12px;" id="e<?= $no;?>">maksimal angka 12 dan minimal 1</p>
+                            <input id="i<?= $no;?>"   onkeyup="change(this.value,<?= $no-1;?>);cekVal();" type="number" name="val[]" data-parsley-trigger="change" required placeholder="max : 12" autocomplete="off" class="form-control">
+                            <p style="color:red;margin-left=12px;" id="e<?= $no-1;?>">maksimal angka 12 dan minimal 1</p>
                            </div>
                         </div>
                         <script type="text/javascript">
                           var error = document.getElementById('e<?= $no;?>').style.display = 'none';
+
                           function change(e,no){
                             var error = document.getElementById('e'+no);
-                            
-                            console.log(e);
+                            n = no;
+                        
                             if(e > 12){
                              error.style.display = 'block';
+                             no[n] = false;
                             }else if(e < 1){
                               error.style.display = 'block';
+                              no[n] = false;
                             }else{
                               error.style.display = 'none';
+                              no[n] = true;
                             }
-
+                            console.log("no"+n)
+                            
                           }
+                         
                         </script>
                         <?php
                           $no++;
@@ -56,7 +67,7 @@
                         <div class="row">      
                             <div class="col-sm-6 pl-0">
                                 <p class="text-right">
-                                  <button type="submit" class="btn btn-space btn-primary">
+                                  <button id="btnsimpan" type="submit" class="btn btn-space btn-primary">
                                   <?php if($_GET['tabel'] == "I"){
                                       echo "Simpan";
                                     }else{
@@ -64,6 +75,30 @@
                                     }?>
                                     </button>
                                 </p>
+                                  <script type="text/javascript">
+                                    var dok = document.getElementById('btnsimpan').disabled = true;
+
+                                  function cekVal(){
+                                   
+                                    // if(no1 == true && 
+                                    // no2 == true && 
+                                    // no3 == true && 
+                                    // no4 == true && 
+                                    // no5 == true && 
+                                    // no6 == true && 
+                                    // no7 == true &&
+                                    // no8 == true &&
+                                    // no9 == true &&
+                                    // no10 == true &&
+                                    // no11 == true &&
+                                    // no12 == true ){
+
+                                    //   console.log("true");
+                                    //   document.getElementById('btnsimpan').disabled = false;
+                                    // }
+                                    console.log(no[1]);
+                                  }
+                                  </script>
                             </div>
                         </div>
                       </form>
